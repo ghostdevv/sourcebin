08451a0cbc5e1a4b9f8f66d6d2ae292085889f23
GHOST <ghostdevbusiness@gmail.com>
2021-01-31 01:53:19 +0000

    feat: fetch helper

---
+++ src/util/fetch.js
@@ -0,0 +1,16 @@
+const { version } = require('../../package.json');
+const got = require('got');
+
+module.exports = async (url, options = {}) => {
+    return got(url, {
+        json: options.data,
+        responseType: 'json',
+
+        ...options,
+
+        headers: {
+            ...options.headers,
+            'User-Agent': `Sourcebin/${version} https://www.npmjs.com/package/sourcebin`,
+        },
+    }).then((res) => res.body);
+};
