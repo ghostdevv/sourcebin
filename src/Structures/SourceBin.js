c13a12abfc7c613b056b33020f48b356220d924b
GHOST <ghostdevbusiness@gmail.com>
2021-01-31 01:53:29 +0000

    feat: sourcebin class

---
+++ src/Structures/SourceBin.js
@@ -0,0 +1,25 @@
+const File = require('./File.js');
+
+class SourceBin {
+    constructor(key, data) {
+        this.key = key;
+
+        this.url = `https://sourceb.in/${key}`;
+        this.short = `https://srcb.in/${key}`;
+
+        this.title = data.title;
+        this.description = data.description;
+        this.views = data.hits;
+
+        this.created = data.created;
+        this.timestamp = new Date(data.created).getTime();
+
+        this.files = [];
+        for (let i = 0; i < data.files.length; i++) {
+            const file = new File(key, i, data.files[i]);
+            this.files.push(file);
+        }
+    }
+}
+
+module.exports = SourceBin;
