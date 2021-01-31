8b2c6d0853c830e7891c66ceb4d71c92ebe5c7d2
GHOST <ghostdevbusiness@gmail.com>
2021-01-31 01:50:50 +0000

    tests: get method test

---
+++ tests/methods/get.test.js
@@ -0,0 +1,18 @@
+const { get } = require('../../src/methods');
+
+const key = 'qXO2NVhRc6';
+
+test('check that get method returns expected response', async () => {
+    const res = await get(key);
+    expect(res);
+});
+
+test('check that fetchContent option works as expects', async () => {
+    const res = await get(key);
+
+    const withOption = await get(key, {
+        fetchContent: false,
+    });
+
+    expect(res).not.toEqual(withOption);
+});
