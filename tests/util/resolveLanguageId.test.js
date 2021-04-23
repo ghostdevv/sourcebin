ecfd87311d7240f2fdd9f4d154d36229d1b7bdba
GHOST <ghostdevbusiness@gmail.com>
2021-04-23 10:08:00 +0100

    tests: update resolveLanguageId test

--- tests/util/resolveLanguageId.test.js
+++ tests/util/resolveLanguageId.test.js
@@ -1,26 +1,31 @@
 const { resolveLanguageId } = require('../../src/util');
 
-test('check that resolveLanguageId returns expected id', () => {
+test('returns expected id', () => {
     const id = resolveLanguageId('JavaScript');
     expect(id).toBe(183);
 });
 
-test('check that resolveLanguageId is case insensitive', () => {
+test('is case insensitive', () => {
     const id = resolveLanguageId('jAvAsCrIpT');
     expect(id).toBe(183);
 });
 
-test('check that resolveLanguageId returns a error if invalid id', () => {
+test('works with alias', () => {
+    const id = resolveLanguageId('js');
+    expect(id).toBe(183);
+});
+
+test('returns undefined if invalid id', () => {
     const id = resolveLanguageId('madeup language');
     expect(id).toBe(undefined);
 });
 
-test("check that resolveLanguageId returns id if it's valid id", () => {
+test("returns id if it's valid number id", () => {
     const id = resolveLanguageId(183);
     expect(id).toBe(183);
 });
 
-test("check that resolveLanguageId returns undefined if it's a invalid id", () => {
+test("returns undefined if it's a invalid number id", () => {
     const id = resolveLanguageId(1908881247891);
     expect(id).toBe(undefined);
 });
