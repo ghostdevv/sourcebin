0fbceeeb7e843bab1fdc441bd39a21ea826f5d90
GHOST <ghostdevbusiness@gmail.com>
2022-09-04 10:51:51 +0000

    feat: test structures

---
+++ tests/structures/bin.test.ts
@@ -0,0 +1,78 @@
+import { File } from '../../src/structures/File';
+import { Bin } from '../../src/structures/Bin';
+import assert from 'assert';
+import { test } from 'uvu';
+
+test('can create bin', () => {
+    const bin = new Bin({
+        key: 'qXO2NVhRc6',
+        created: '2021-01-31T00:34:35.635Z',
+        hits: 362,
+        title: 'test title',
+        description: 'test description',
+        files: [
+            {
+                index: 0,
+                languageId: 222,
+                content: 'test',
+                name: 'test name',
+            },
+        ],
+    });
+
+    assert.ok(bin);
+});
+
+test('has expected fields', () => {
+    const bin = new Bin({
+        key: 'qXO2NVhRc6',
+        created: '2021-01-31T00:34:35.635Z',
+        hits: 362,
+        title: 'test title',
+        description: 'test description',
+        files: [
+            {
+                index: 0,
+                languageId: 222,
+                content: 'test',
+                name: 'test name',
+            },
+        ],
+    });
+
+    assert.equal(bin.key, 'qXO2NVhRc6');
+    assert.equal(bin.created.toISOString(), '2021-01-31T00:34:35.635Z');
+    assert.equal(bin.views, 362);
+    assert.equal(bin.title, 'test title');
+    assert.equal(bin.description, 'test description');
+});
+
+test('has files', () => {
+    const bin = new Bin({
+        key: 'qXO2NVhRc6',
+        created: '2021-01-31T00:34:35.635Z',
+        hits: 362,
+        title: 'test title',
+        description: 'test description',
+        files: [
+            {
+                index: 0,
+                languageId: 222,
+                content: 'test',
+                name: 'test name',
+            },
+        ],
+    });
+
+    assert.deepEqual(
+        bin.files[0],
+        new File('qXO2NVhRc6', {
+            index: 0,
+            languageId: 222,
+            content: 'test',
+            name: 'test name',
+        }),
+    );
+});
+
+test.run();
